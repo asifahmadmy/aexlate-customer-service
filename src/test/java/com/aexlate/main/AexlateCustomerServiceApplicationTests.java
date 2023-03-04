0a670f7fabec0e228a3066d633ebb4a6a391325b
asifahmadmy <asif-ahmad@hotmail.com>
2023-03-04 23:24:34 +0800

    initial commit

---
+++ src/test/java/com/aexlate/main/AexlateCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aexlate.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AexlateCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
