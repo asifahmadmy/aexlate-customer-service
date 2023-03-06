81e406fd532a17f1e21cac4196c8793da954dc26
asifahmadmy <asif-ahmad@hotmail.com>
2023-03-06 18:43:04 +0800

    updated application.yml

--- src/main/java/com/aexlate/AexlateCustomerServiceApplication.java
+++ src/main/java/com/aexlate/AexlateCustomerServiceApplication.java
@@ -2,8 +2,11 @@ package com.aexlate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class AexlateCustomerServiceApplication {
 
 	public static void main(String[] args) {
